fc814846555f1d2608e0f5cb864a8b2e43850d3e
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2020-01-10 11:16:06 +0100

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,19 +1,25 @@
-def is_palindrome(text: str):
+def is_palindrome(text: str) -> bool:
+    """
+    Function checks if the text and reverse are identical
+    """
     return text == text[::-1]
-
-data = input().lower()
-out = False
 
 if is_palindrome(data):
     out = True
 
 else:
     for i in range(len(data)):
-        if is_palindrome(data[:i] + data[i+1:]): out = True
-        
+        if is_palindrome(data[:i] + data[i+1:]):
+            out = True
+            break
+            
     if not out:
         for i in range(len(data)):
             for j in range(len(data)-i):
-                if is_palindrome(data[:i] + data[i+1:i+j] + data[i+j+1:]): out = True 
+                if is_palindrome(data[:i] + data[i+1:i+j] + data[i+j+1:]):
+                    out = True 
+                    break
+                    break
+        
 
 print("YES" if out else "NO")
