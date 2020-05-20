76790d9c7986e90e9a4d31e6f1ecceea82399c96
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2020-05-20 09:37:55 +0200

    Fix things

--- backer/test.py
+++ backer/test.py
@@ -11,14 +11,11 @@ for i in teda:
     print(i, end="; ", file=stderr)
     graph = Generator(i, 0.3).list
 
-    timer = 0
     for i in range(3):
         start = time()
         hamilton(graph)
         stop = time()
         timer += (stop - start)
-
-    print(timer / 3, end="; ", file=stderr)
 
     print(file=stderr)
 
@@ -27,7 +24,6 @@ for i in teda:
     print(i, end="; ", file=stderr)
     graph = Generator(i, 0.7).list
 
-    timer = 0
     for i in range(3):
         start = time()
         hamilton(graph)
@@ -43,13 +39,10 @@ for i in [20, 25, 30]:
     print(i, end="; ", file=stderr)
     graph = Generator(i, 0.5, False).list
 
-    timer = 0
     for i in range(3):
         start = time()
         hamilton(graph)
         stop = time()
         timer += (stop - start)
-
-    print(timer / 3, end="; ", file=stderr)
 
     print(file=stderr)
