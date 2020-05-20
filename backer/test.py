683b8e09112a64e2b65a19f7d927c4564ea600e5
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2020-05-20 09:01:44 +0200

    Fix ranges

--- backer/test.py
+++ backer/test.py
@@ -4,10 +4,44 @@ from time import time
 from .ggen import Generator
 from .hamilton import hamilton
 
+teda = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+
 print("Hamilton30", end="; ", file=stderr)
-for i in [10, 20, 30, 40, 50, 55, 60, 65, 70, 75]:
+for i in teda:
     print(i, end="; ", file=stderr)
     graph = Generator(i, 0.3).list
+
+    timer = 0
+    for i in range(3):
+        start = time()
+        hamilton(graph)
+        stop = time()
+        timer += (stop - start)
+
+    print(timer / 3, end="; ", file=stderr)
+
+    print(file=stderr)
+
+print("Hamilton70", end="; ", file=stderr)
+for i in teda:
+    print(i, end="; ", file=stderr)
+    graph = Generator(i, 0.7).list
+
+    timer = 0
+    for i in range(3):
+        start = time()
+        hamilton(graph)
+        stop = time()
+        timer += (stop - start)
+
+    print(timer / 3, end="; ", file=stderr)
+
+    print(file=stderr)
+
+print("Hamilton50", end="; ", file=stderr)
+for i in [20, 25, 30]:
+    print(i, end="; ", file=stderr)
+    graph = Generator(i, 0.5, False).list
 
     timer = 0
     for i in range(3):
