34bee093cc9846fa220d9a79eaebd936a0c180b3
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2020-05-20 09:43:29 +0200

    Fix fixing things

--- backer/test.py
+++ backer/test.py
@@ -6,7 +6,7 @@ from .hamilton import hamilton
 
 teda = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 
-print("Hamilton30", end="; ", file=stderr)
+print("Hamilton30", file=stderr)
 for i in teda:
     print(i, end="; ", file=stderr)
     graph = Generator(i, 0.3).list
@@ -15,11 +15,12 @@ for i in teda:
         start = time()
         hamilton(graph)
         stop = time()
-        timer += (stop - start)
+        timer = (stop - start)
+        print(timer, end="; ", file=stderr)
 
     print(file=stderr)
 
-print("Hamilton70", end="; ", file=stderr)
+print("Hamilton70", file=stderr)
 for i in teda:
     print(i, end="; ", file=stderr)
     graph = Generator(i, 0.7).list
@@ -28,13 +29,12 @@ for i in teda:
         start = time()
         hamilton(graph)
         stop = time()
-        timer += (stop - start)
-
-    print(timer / 3, end="; ", file=stderr)
+        timer = (stop - start)
+        print(timer, end="; ", file=stderr)
 
     print(file=stderr)
 
-print("Hamilton50", end="; ", file=stderr)
+print("Hamilton50", file=stderr)
 for i in [20, 25, 30]:
     print(i, end="; ", file=stderr)
     graph = Generator(i, 0.5, False).list
@@ -43,6 +43,7 @@ for i in [20, 25, 30]:
         start = time()
         hamilton(graph)
         stop = time()
-        timer += (stop - start)
+        timer = (stop - start)
+        print(timer, end="; ", file=stderr)
 
     print(file=stderr)
